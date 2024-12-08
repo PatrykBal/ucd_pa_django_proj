a5f10c35b9d26bf267b2d1cbba4c941ca671c87c
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-12-08 23:20:27 +0000

    populated models.py, updated admin.py, urls.py, views.py also added forms.py and project_form.html

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
+from .forms import ProjectForm
 
 def projects(request):
-    page = 'projects'
-    number = 11
-    context = {'page':page, 'number':number}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-projects.html')
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-projects.html', {'project':projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
 
