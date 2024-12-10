5bf2205c74bf1dfcf3f361f7819899f422485725
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-12-10 07:37:37 +0000

    costumised registeration form, populated forms.py

--- users/views.py
+++ users/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
 from .models import Profile
 
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -37,6 +39,28 @@ def logoutUser(request):
     logout(request)
     messages.error(request, 'User was successfully logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.success(request, 'Registration Error')
+
+    context = {'page': page, 'form':form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
